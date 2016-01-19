4200f7b8b598ba7c52089ecbd6d8bac60bbe5687
Viksit Arora <viksit@mit.edu>
2016-01-20 00:47:06 +0530

    removes devise routes except sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, only: [:sign_in]
+  
   resources  :sessions, only: [:create, :destroy]
 
   resources  :users, only: [:show, :create, :update, :destroy] do
