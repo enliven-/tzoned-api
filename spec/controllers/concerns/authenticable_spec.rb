1a10bcc46934a8988dd0f35044438ea27d2fb9d8
Viksit Arora <viksit@mit.edu>
2016-01-19 23:08:17 +0530

    adds authenticable module to be used for authentication; adds spec for same

---
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+
+class Authentication
+  include Authenticable
+
+  def request
+  end
+
+  def response
+  end
+end
+
+
+RSpec.describe Authenticable do
+
+  let(:authentication) { Authentication.new }
+  subject { authentication }
+
+  describe '#current_user' do
+    before do
+      @user = FactoryGirl.create :user
+      request.headers['Authorization'] = @user.auth_token
+      allow(authentication).to receive(:request) { request }
+    end
+    it 'returns the user from the authorization header' do
+      expect(authentication.current_user.auth_token).to eql @user.auth_token
+    end
+  end
+
+
+  describe '#authenticate_with_token' do
+    before do
+      @user = FactoryGirl.create :user
+      allow(authentication).to receive(:current_user) { nil }
+      allow(response).to receive(:response_code) { 401 }
+      allow(response).to receive(:body) { {'errors' => 'Not authenticated'}.to_json }
+      allow(authentication).to receive(:response) { response }
+    end
+
+    it 'render a json error message' do
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(json_response[:errors]).to eql 'Not authenticated'
+    end
+
+    it {  should respond_with 401 }
+  end
+end 
