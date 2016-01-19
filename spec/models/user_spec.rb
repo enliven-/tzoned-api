bcce064654dafb824a3221cca28fb222ab59bc67
Viksit Arora <viksit@mit.edu>
2016-01-19 22:48:05 +0530

    adds auth_token to user; adds spec for same

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
     expect(user).to respond_to(:email)
     expect(user).to respond_to(:password)
     expect(user).to respond_to(:password_confirmation)
+    expect(user).to respond_to(:auth_token)
   end
 
 
@@ -19,7 +20,7 @@ RSpec.describe User, type: :model do
   end
 
 
-  it 'should validate presence of email & password' do
+  it 'validates presence of email & password' do
     user = FactoryGirl.build(:user)
     
     expect(user).to validate_presence_of(:email)
@@ -27,10 +28,43 @@ RSpec.describe User, type: :model do
   end
 
 
+  it 'validates uniqueness of auth_token' do
+    user = FactoryGirl.build(:user)
+
+    expect(user).to validate_uniqueness_of(:auth_token)
+  end
+
+
   it 'should validate case-insensitive uniqueness of email' do
     user = FactoryGirl.build(:user)
 
     expect(user).to validate_uniqueness_of(:email).case_insensitive
+  end
+
+
+  describe '#generate_auth_token!' do
+    before(:each) { @user = FactoryGirl.create(:user) }
+
+    it 'generates a token' do
+      @user.generate_auth_token!
+
+      expect(@user.auth_token).not_to be_empty
+    end
+
+    it 'generates the token via devise friendly token helper' do
+      allow(Devise).to receive(:friendly_token) { 'authtoken123' }
+      @user.generate_auth_token!
+
+      expect(@user.auth_token).to eql('authtoken123')
+    end
+
+    it 'generates another token when one already has been taken' do
+      @user.generate_auth_token!
+      existing_token = @user.auth_token
+      new_user = FactoryGirl.create(:user, auth_token: existing_token)
+
+      expect(new_user.auth_token).not_to eql @user.auth_token
+    end
   end
 
 end
