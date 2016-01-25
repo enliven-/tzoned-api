2794cf6851ddd66eb1d975637ecd5d90dc6c99c9
Viksit Arora <viksit@mit.edu>
2016-01-25 22:37:17 +0530

    removes timezone validation for being an integer

--- app/models/timezone.rb
+++ app/models/timezone.rb
@@ -3,7 +3,7 @@ class Timezone < ActiveRecord::Base
   validates :abbr, presence: true, uniqueness: { case_sensitive: false }
   validates :city, presence: true, uniqueness: { case_sensitive: false }
   validates :gmt_difference, presence: true,
-            numericality: { only_integer: true, greater_than_or_equal_to: -43200, less_than_or_equal_to: 43200 }
+            numericality: { greater_than_or_equal_to: -43200, less_than_or_equal_to: 43200 }
 
   belongs_to :user
 
